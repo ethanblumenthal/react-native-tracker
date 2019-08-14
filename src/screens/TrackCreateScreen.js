d6f9745da0c3e4b11892bde03a89883d402ca0e5
ethanblumenthal <ethan.blumenthal@gmail.com>
2019-08-13 22:25:47 -0500

    created map component and push notification for location permission

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,8 +1,32 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet } from 'react-native';
+import { Text } from 'react-native-elements';
+import { SafeAreaView } from 'react-navigation';
+import { requestPermissionsAsync } from 'expo-location';
+import Map from '../components/Map';
 
 const TrackCreateScreen = () => {
-  return <Text style={{ fontSize: 48 }}>TrackCreateScreen</Text>;
+  const [err, setErr] = useState(null);
+
+  const startWatching = async () => {
+    try {
+      await requestPermissionsAsync();
+    } catch (e) {
+      setErr(e);
+    }
+  };
+
+  useEffect(() => {
+    startWatching();
+  }, []);
+
+  return (
+    <SafeAreaView forceInset={{ top: 'always' }}>
+      <Text>Create a Track</Text>
+      <Map />
+      {err ? <Text>Please enable location services</Text> : null}
+    </SafeAreaView>
+  );
 };
 
 const styles = StyleSheet.create({});
