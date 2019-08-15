df558b9ec5b718dc2f3ab323738b2dc9124cf9e3
ethanblumenthal <ethan.blumenthal@gmail.com>
2019-08-14 20:47:37 -0500

    implemented ability to track location over time

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -2,24 +2,26 @@ import '../_mockLocation';
 import React, { useContext } from 'react';
 import { StyleSheet } from 'react-native';
 import { Text } from 'react-native-elements';
-import { SafeAreaView } from 'react-navigation';
+import { SafeAreaView, withNavigationFocus } from 'react-navigation';
 import Map from '../components/Map';
 import { Context as LocationContext } from '../context/LocationContext';
 import useLocation from '../hooks/useLocation';
+import TrackFrom from '../components/TrackForm';
 
-const TrackCreateScreen = () => {
-  const { addLocation } = useContext(LocationContext);
-  const [err] = useLocation(addLocation);
+const TrackCreateScreen = ({ isFocused }) => {
+  const { state, addLocation } = useContext(LocationContext);
+  const [err] = useLocation(isFocused, location => addLocation(location, state.recording));
 
   return (
     <SafeAreaView forceInset={{ top: 'always' }}>
       <Text>Create a Track</Text>
       <Map />
       {err ? <Text>Please enable location services</Text> : null}
+      <TrackFrom />
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({});
 
-export default TrackCreateScreen;
+export default withNavigationFocus(TrackCreateScreen);
